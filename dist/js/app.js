ed5b87f8136cb5ad5182c29d3c74de5dac119080
Tyler Bussanich <tbussanich@gmail.com>
2018-01-12 15:04:42 -0800

    add api call that will return provider name, bio, address, and phone number.

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,16 +7,38 @@ exports.apiKey = "f8086f537604cc1c82533c62b2df8c8b";
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function () {
-  $('#search-results').submit(function () {
+  $('#form').submit(function (event) {
+    event.preventDefault();
     var symptoms = $('#user-symptoms').val();
     var firstName = $('#doctor-first-name').val();
     var lastName = $('#doctor-last-name').val();
     $('#user-symptoms').val("");
     $.ajax({
-      url: 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=' + firstName + '&last_name=' + lastName + '&query=' + symptoms + '&location=or-portland&skip=0&limit=10&user_key=f8086f537604cc1c82533c62b2df8c8b',
+      url: 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=' + firstName + '&last_name=' + lastName + '&query=' + symptoms + '&location=or-portland&skip=0&limit=10&user_key=' + apiKey,
       type: 'GET',
       data: {
         format: 'json'
+      },
+      success: function success(response) {
+        var body = response.data;
+        for (var i = 0; i < body.length; i++) {
+          var _firstName = body[i].profile.first_name;
+          var _lastName = body[i].profile.last_name;
+          var bio = body[i].profile.bio;
+          var street = body[i].practices[0].visit_address.street;
+          var city = body[i].practices[0].visit_address.city;
+          var state = body[i].practices[0].visit_address.state;
+          var zip = body[i].practices[0].visit_address.zip;
+          var phone = body[i].practices[0].phones[0].number;
+          var newPatient = body[i].practices[0].accepts_new_patients;
+
+          $('#form-output').append('\n            <ul>\n            <li>Provider: ' + _firstName + ' ' + _lastName + '</li>\n            <li>Bio: ' + bio + '</li>\n            <li>Address: ' + street + ' ' + city + ' ' + state + ' ' + zip + '</li>\n            <li>Phone: ' + phone + '</li>\n            <li>Accepting New Patients: ' + newPatient + '</li>\n            </ul>');
+        }
+
+        console.log(response.data);
+      },
+      error: function error() {
+        $('#error').text("There was an error processing your request.");
       }
 
     });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,17 +1,46 @@
 const apiKey = require('./../.env').apiKey;
 
 $(document).ready(function()  {
-  $('#search-results').submit(function() {
+  $('#form').submit(function(event) {
+    event.preventDefault();
     let symptoms = $('#user-symptoms').val();
     let firstName = $('#doctor-first-name').val();
     let lastName = $('#doctor-last-name').val();
     $('#user-symptoms').val("");
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${symptoms}&location=or-portland&skip=0&limit=10&user_key=f8086f537604cc1c82533c62b2df8c8b`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${symptoms}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`,
       type: 'GET',
       data: {
         format: 'json',
       },
+      success: function(response) {
+        let body = response.data;
+        for (let i = 0; i < body.length; i++) {
+          const firstName = body[i].profile.first_name;
+          const lastName = body[i].profile.last_name;
+          const bio = body[i].profile.bio;
+          const street = body[i].practices[0].visit_address.street;
+          const city = body[i].practices[0].visit_address.city;
+          const state = body[i].practices[0].visit_address.state;
+          const zip = body[i].practices[0].visit_address.zip;
+          const phone = body[i].practices[0].phones[0].number;
+          const newPatient = body[i].practices[0].accepts_new_patients;
+
+          $('#form-output').append(`
+            <ul>
+            <li>Provider: ${firstName} ${lastName}</li>
+            <li>Bio: ${bio}</li>
+            <li>Address: ${street} ${city} ${state} ${zip}</li>
+            <li>Phone: ${phone}</li>
+            <li>Accepting New Patients: ${newPatient}</li>
+            </ul>`);
+        }
+
+        console.log(response.data);
+      },
+      error: function() {
+        $('#error').text("There was an error processing your request.");
+      }
 
     })
   })
