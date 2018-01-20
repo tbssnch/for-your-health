043b7a627b8dc4413a2f2112ab1c4e24ade0d5b1
Tyler Bussanich <tbussanich@gmail.com>
2018-01-20 13:43:24 -0800

    separate front and back end

--- src/js/health.js
+++ src/js/health.js
@@ -1,42 +1,11 @@
-export { loadDoctorsSuccess, loadDoctorsError };
+import { loadDoctorsSuccess, loadDoctorsError } from './../src/js/health-interface.js';
 
-function loadDoctorsSuccess(response) {
-  response.data.map(function(doctorInfo) {
-    const firstName = doctorInfo.profile.first_name;
-    const lastName = doctorInfo.profile.last_name;
-    const street = doctorInfo.practices[0].visit_address.street;
-    const city = doctorInfo.practices[0].visit_address.city;
-    const state = doctorInfo.practices[0].visit_address.state;
-    const zip = doctorInfo.practices[0].visit_address.zip;
-    const phone = doctorInfo.practices[0].phones[0].number;
-    let website = "";
-    let newPatient = "";
-
-    if (doctorInfo.practices[0].website) {
-      website = `<li>Website: ${doctorInfo.practices[0].website}</li>`;
-    }
-
-    if (doctorInfo.practices[0].accepts_new_patients === true) {
-      newPatient = `<li>Accepting New Patients: Yes</li>`;
-    }
-    else if (doctorInfo.practices[0].accepts_new_patients === false) {
-      newPatient = `<li>Accepting New Patients: No</li>`;
-    }
-
-    $('#form-output').append(`
-      <div class="results">
-        <ul>
-        <li>Provider: ${firstName} ${lastName}</li>
-        <li>Address: ${street} ${city} ${state} ${zip}</li>
-        <li>Phone: ${phone}</li>
-        ${newPatient}
-        ${website}
-        </ul>
-      </div>
-    `);
-  });
-}
-
-function loadDoctorsError(response) {
-  $('#error').text("There was an error processing your request.");
-}
+$.ajax({
+  url: `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${symptoms}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`,
+  type: 'GET',
+  data: {
+    format: 'json',
+  },
+  success: loadDoctorsSuccess,
+  error: loadDoctorsError,
+});
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,6 +1,47 @@
 const apiKey = require('./../.env').apiKey;
 import { loadDoctorsSuccess, loadDoctorsError } from './../src/js/health.js';
 
+function loadDoctorsSuccess(response) {
+  response.data.map(function(doctorInfo) {
+    const firstName = doctorInfo.profile.first_name;
+    const lastName = doctorInfo.profile.last_name;
+    const street = doctorInfo.practices[0].visit_address.street;
+    const city = doctorInfo.practices[0].visit_address.city;
+    const state = doctorInfo.practices[0].visit_address.state;
+    const zip = doctorInfo.practices[0].visit_address.zip;
+    const phone = doctorInfo.practices[0].phones[0].number;
+    let website = "";
+    let newPatient = "";
+
+    if (doctorInfo.practices[0].website) {
+      website = `<li>Website: ${doctorInfo.practices[0].website}</li>`;
+    }
+
+    if (doctorInfo.practices[0].accepts_new_patients === true) {
+      newPatient = `<li>Accepting New Patients: Yes</li>`;
+    }
+    else if (doctorInfo.practices[0].accepts_new_patients === false) {
+      newPatient = `<li>Accepting New Patients: No</li>`;
+    }
+
+    $('#form-output').append(`
+      <div class="results">
+        <ul>
+        <li>Provider: ${firstName} ${lastName}</li>
+        <li>Address: ${street} ${city} ${state} ${zip}</li>
+        <li>Phone: ${phone}</li>
+        ${newPatient}
+        ${website}
+        </ul>
+      </div>
+    `);
+  });
+}
+
+function loadDoctorsError(response) {
+  $('#error').text("There was an error processing your request.");
+}
+
 $(document).ready(function()  {
   $('#form').submit(function(event) {
     event.preventDefault();
@@ -15,7 +56,7 @@ $(document).ready(function()  {
       data: {
         format: 'json',
       },
-      success: loadDoctorsSuccess,  
+      success: loadDoctorsSuccess,
       error: loadDoctorsError,
     });
   });
